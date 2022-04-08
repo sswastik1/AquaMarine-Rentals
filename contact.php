2e5d1dc27442d2d1aff9478574eb5ee75aa5e0a0
SCORPION <coder@swastiks-MacBook-Pro.local>
2022-04-07 19:30:40 -0600

    AquaMarine

---
+++ contact.php
@@ -0,0 +1,257 @@
+
+
+<html lang="en">
+   <head>
+      <title>Contact Us</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Dancing+Script:400,700|Muli:300,400" rel="stylesheet">
+      <link rel="stylesheet" href="css/all.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+      <!-- slick CSS -->
+      <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+      <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+      <!-- Add icon library -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   </head>
+   <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300" data-aos-easing="slide" data-aos-duration="800" data-aos-delay="0" data-new-gr-c-s-check-loaded="14.1049.0" data-gr-ext-installed="">
+      <div class="site-wrap">
+          <header>
+            <div class="container">
+               <nav class="navbar navbar-expand-lg bg-transparent">
+                  <a class="navbar-brand" href="home.html"> <img src="img/logo.png" class="img-fluid" alt="Logo"> </a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon fa fa-bars"></span>
+                  </button>
+                  <div class="collapse navbar-collapse justify-content-end phone_bgwhite" id="navbarNavAltMarkup">
+                     <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="home.html">Home <span class="sr-only">(current)</span></a>
+                        <!-- Example single danger button -->
+                        <div class="btn-group">
+                           <button type="button" class="nav-item nav-link dropdown-toggle bg-transparent" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Our Boats
+                           </button>
+                           <div class="dropdown-menu">
+                           <a class="dropdown-item" href="boat_1.html">2016 Four Winns Freedom 190</a>
+                           <a class="dropdown-item" href="boat_2.html">Sea Doo Wake Pro</a>
+                           <a class="dropdown-item" href="boat_3.html">2021 Princecraft Vectra</a>
+                           </div>
+                        </div>
+                        <a class="nav-item nav-link" href="rental_policy.html">Rental Policy</a>
+                        <a class="nav-item nav-link" href="contact.html">Contact Us</a>
+                        <a type="button" class="nav-item nav-linkbtn btn-white ml-4" data-toggle="modal" data-target="#booknow_form">Book Now</a>
+                     </div>
+                  </div>
+               </nav>
+            </div>
+         </header>
+
+      <!-- Modal -->
+      <div class="modal fade" id="booknow_form" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+         <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header d-block">
+               <h3 class="heading-92913 text-dark mb-0">Book Now</h3>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+               <div class="row mx-0">
+                  <div class="col-md-12 mx-auto px-0 bg-white">
+                    <div class="site-section booking_area bg-white contact_us py-5" style="background-image: unset;padding-bottom: 0px !important;" data-aos="fade-right">
+                           <div class="row mx-0">
+                              <div class="col-md-12 mx-auto">
+                                 <form action="#" class="row">
+                                    <div class="form-group col-md-6">
+                                       <label for="input-1">Full Name:</label>
+                                       <input type="text" class="form-control" id="input-1">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                       <label for="input-6">Email Address</label>
+                                       <input type="text" class="form-control" id="input-6">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                       <label for="input-7">Phone Number</label>
+                                       <input type="text" class="form-control" id="input-7">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="input-5">Yacht You're Interested in:</label>
+                                        <select name="" id="input-5" class="form-control">
+                                           <option value="">Motor Yacht</option>
+                                           <option value="">Hi-Speed Yacht</option>
+                                           <option value="">Premium Yacht</option>
+                                           <option value="">Presidential Yacht</option>
+                                        </select>
+                                     </div>
+                                    <div class="form-group col-md-12">
+                                       <label for="input-8">Notes</label>
+                                       <textarea name="" id="input-8" cols="30" rows="5" class="form-control"></textarea>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                       <input type="submit" class="btn btn-primary py-3 px-5" value="Submit">
+                                    </div>
+                                 </form>
+                              </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </div>
+            <div id="banner" class="inner_banner d-flex flex-wrap align-items-center position-relative contacts w-100">
+                    <div class="slide_main w-100 position-relative">
+                        <div class="row">
+                            <div class="col-md-12" data-aos="fade-up">
+                                <div class="inner text-center w-lg-50 text-white">
+                                    <h1><b>Contact Us</b></h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="row mx-0">
+                <div class="col-md-8 mx-auto px-0 bg-white">
+                    <div class="site-section booking_area bg-white contact_us py-5" style="background-image: unset;" data-aos="fade-right">
+                       
+                           <div class="row mx-0">
+                              <div class="col-md-10 mx-auto">
+                                 <span class="text-serif text-dark">Contact Us</span>
+                                 <h3 class="heading-92913 text-dark">Feel Free to Contact Us</h3>
+                                 <form method="Post" action="contactMail.php" class="row">
+                                    <div class="form-group col-md-6">
+                                       <label for="input-1">Full Name:</label>
+                                       <input type="text" name="name" class="form-control" id="input-1" required  pattern="[a-zA-Z ]+">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                       <label for="input-6">Email Address</label>
+                                       <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"  class="form-control" id="input-6" name="email" required="true">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                       <label for="input-7">Phone Number</label>
+                                       <input type="text"  pattern="[0-9]{8,12}+" class="form-control" id="input-7" name="phone" required="true">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="input-5">Yacht You're Interested in:</label>
+                                        <select name="type" id="input-5" class="form-control" required="true">
+                                           <option value="Motor Yacht">Motor Yacht</option>
+                                           <option value="Hi-Speed Yacht">Hi-Speed Yacht</option>
+                                           <option value="Premium Yacht">Premium Yacht</option>
+                                           <option value="Presidential Yacht">Presidential Yacht</option>
+                                        </select>
+                                     </div>
+                                    <div class="form-group col-md-12">
+                                       <label for="input-8">Other Info</label>
+                                       <textarea name="description" id="input-8" cols="30" rows="5" class="form-control"></textarea>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                       <input type="submit" class="btn btn-primary py-3 px-5" value="Submit">
+                                    </div>
+                                 </form>
+                              </div>
+                        </div>
+                     
+                     </div>
+                </div>
+                <div class="col-md-4 bg-light px-0 d-flex flex-wrap align-items-center">
+                    <div class="inner contact_details  py-5 px-4" data-aos="fade-left">
+                        <div class="contacts-list ">
+                            <ul class="no-bullets">
+                               <li class="contact-item">
+                                  <span class="title bold">Phone</span>
+                                  <span class="desc">
+                                  <a href="tel:7808071487">7808071487</a></span>
+                               </li>
+                               <li class="contact-item">
+                                  <span class="title bold">
+                                  Email                    </span>
+                                  <span class="desc">
+                                  <a href="mailto:info@aquamarinerentals.ca">info@aquamarinerentals.ca</a>                    </span>
+                               </li>
+                               <li class="contact-item">
+                                  <span class="title bold">
+                                  Address</span>
+                                  <span class="desc">3652 74 Ave NW, Edmonton, AB T6B 2P7</span>
+                               </li>
+                               <li class="contact-item">
+                                  <span class="title bold">
+                                  Hours                    </span>
+                                  <span class="desc">
+                                    8 am – 6pm</span>
+                               </li>
+                            </ul>
+                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2372.6601169889614!2d-113.39943788460792!3d53.51026667012204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53a0186b4fcd3139%3A0xe093615b54ad1c0!2s3652%2074%20Ave%20NW%2C%20Edmonton%2C%20AB%20T6B%202P7%2C%20Canada!5e0!3m2!1sen!2sin!4v1646672693385!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+         <div class="footer bg-light">
+            <div class="container">
+               <!-- <div class="row">
+                     <div class="col-lg-3">
+                        <h2>Logo Here</h2> 
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo minima qui dolor, iusto iure.</p>
+                     </div>
+                     <div class="col-lg-3">
+                        <h3 class="footer-heading"><span>Our Company</span></h3>
+                        <ul class="list-unstyled">
+                           <li><a href="#">About</a></li>
+                           <li><a href="#">News</a></li>
+                           <li><a href="#">Services</a></li>
+                           <li><a href="#">Our Team</a></li>
+                           <li><a href="#">Careers</a></li>
+                           <li><a href="#">Projects</a></li>
+                        </ul>
+                     </div>
+                     <div class="col-lg-3">
+                        <h3 class="footer-heading"><span>Our Services</span></h3>
+                        <ul class="list-unstyled">
+                           <li><a href="#">Industrial</a></li>
+                           <li><a href="#">Construction</a></li>
+                           <li><a href="#">Remodeling</a></li>
+                        </ul>
+                     </div>
+                     <div class="col-lg-3">
+                        <h3 class="footer-heading"><span>Contact</span></h3>
+                        <ul class="list-unstyled">
+                           <li><a href="#">Help Center</a></li>
+                           <li><a href="#">Support Community</a></li>
+                           <li><a href="#">Press</a></li>
+                           <li><a href="#">FAQ</a></li>
+                           <li><a href="#">Our Partners</a></li>
+                        </ul>
+                     </div>
+                  </div> -->
+              <div class="row">
+                  <div class="col-12">
+                     <div class="copyright">
+                        <p>
+                           Copyright ©2022 All rights reserved
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <ul class="social_icons">
+         <li><a href="https://www.facebook.com/AquaMarine-Rentals-101177769200318/"><i class="fa fa-facebook-f"></i></a></li>
+         <li><a href="https://instagram.com/aquamarine_rentals?utm_medium=copy_link"><i class="fa fa-instagram"></i></a></li>
+      </ul>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+      <script>
+         AOS.init();
+      </script>
+      <script type="text/javascript" src="js/slick.min.js"></script>
+   </body>
+</html>
